9da0f971c3315a8ed127e379418f878347ebb131
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2024-04-23 13:59:40 +0400

    spring_layout

---
+++ src/main/java/com/example/layoutpagetitle/LayoutPageTitleApplication.java
@@ -0,0 +1,13 @@
+package com.example.layoutpagetitle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayoutPageTitleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayoutPageTitleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/layoutpagetitle/LayoutPageTitleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.layoutpagetitle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayoutPageTitleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
